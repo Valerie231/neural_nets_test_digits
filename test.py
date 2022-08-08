3afdbeeb11e63817efe50fe0d98f885f24dc29c0
Valerie <Valerie@MacBook-Pro-Macbook.local>
2022-08-08 22:22:04 +0200

    add sigmoid function

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+"""
+Test of neural nets written exlusively on numpy
+"""
+
+import numpy as np
+
+
 def preprocessing(path):
     """
     The function returns preprocessed dataset, 
@@ -12,7 +19,16 @@ def preprocessing(path):
         dataset.append(instance)
     return dataset
 
+
+def sigmoid(z):
+    """
+    Activation function
+    """
+    return 1.0/(1.0+np.exp(-z))
+
+
 if __name__=="__main__":
     a=preprocessing("optdigits.tra")
     print(a[0])
+
 
