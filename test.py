9140545a9470bb56ed372477bcacbea45a64aeae
ptakha <potoka1998@gmail.com>
2022-08-08 21:57:34 +0200

    Preprocessing function: add squishing of input to range of 0:1

--- test.py
+++ test.py
@@ -3,10 +3,13 @@ def preprocessing(path):
     The function returns preprocessed dataset, 
     taking path to dataset as input
     """
+    dataset=[]
     with open(path, "r") as f:
-        dataset = f.readlines()
-    for instance in dataset:
+        dataset_raw = f.readlines()
+    for instance in dataset_raw:
         instance=list(map(int, instance[:-1].split(",")))
+        instance[:-1]=[el/16 for el in instance[:-1]]
+        dataset.append(instance)
     return dataset
 
 if __name__=="__main__":
