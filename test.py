c14d8078bc6e0100c448031775fa5826857bf399
Valerie <Valerie@MacBook-Pro-Macbook.local>
2022-08-14 18:05:30 +0200

    update

--- test.py
+++ test.py
@@ -41,26 +41,27 @@ class Network(object):
         """
         self.nr_layers = len(sizes)
         self.sizes = sizes
-        self.layers = np.array([np.zeros(i) for i in self.sizes], dtype=object)
+        self.layers = np.array([np.zeros((i,1)) for i in sizes], dtype=object)
         weight_sizes=list(zip(self.sizes[1:], self.sizes))
-        self.weights = [np.random.rand(el[0], el[1]) for el in weight_sizes]
-        self.bias = [np.random.rand(el) for el in self.sizes[1:]]
+        self.weights = np.array([np.random.rand(el[0], el[1]) for el in weight_sizes], dtype=object)
+        self.bias = np.array([np.random.rand(el, 1) for el in self.sizes[1:]], dtype=object)
         self.z = np.array([np.zeros(i) for i in self.sizes], dtype=object)
-    
+        
     def feedforward(self, input):
         """
         neural network applying for our input data
         """
-        self.layers[0]=input
+        self.layers[0]=np.array([[i] for i in input])
         for y in range(1,len(self.sizes)):
             self.z[y]=np.dot(self.weights[y-1], self.layers[y-1]) + self.bias[y-1]
             self.layers[y] = sigmoid(self.z[y]) 
+            
 
     def cost(self, expected_output):
         """
         cost function
         """
-        output=self.layers[-1]
+        output=self.layers[-1][0]
         cost = 0.5*(output - expected_output)**2/self.sizes[-1]
         return cost
     
@@ -72,16 +73,21 @@ class Network(object):
         delta_b=[]
         delta_w=[]
         self.feedforward(input)
+        expected_output = np.array([[i] for i in expected_output])
         cost = self.cost(expected_output)
         delta = (self.layers[-1]-expected_output)/self.sizes[-1]*sigmoid_prime(self.z[-1])
-        delta_b.append(delta)
-        for i in reversed(range(1, self.nr_layers-1)):
-            print(i)
-            delta_w.append(np.dot(self.layers[i-1], delta))
-            delta = np.dot(np.transpose(self.weights[i]), delta) * sigmoid_prime(self.z[i-1])
+        for i in reversed(range(1, self.nr_layers)):
+            delta_w.append(np.dot(delta, self.layers[i-1].T))
             delta_b.append(delta)
-        delta_b=list(reversed(delta_b))
-        delta_w=list(reversed(delta_w))
+            delta = np.dot(np.transpose(self.weights[i-1]), delta) * sigmoid_prime(self.z[i-1])
+        delta_b_list=list(reversed(delta_b))
+        delta_w_list=list(reversed(delta_w))
+        delta_b=np.array([[el] for el in delta_b_list], dtype=object)
+        delta_w=np.array([[el] for el in delta_w_list], dtype=object)
+        print(type(delta_b))
+        print(delta_b[0].shape)
+        print(type(self.bias))
+        print(self.bias[0].shape)
         self.bias=self.bias+learn_rate*delta_b
         self.weights=self.weights+learn_rate*delta_w
         return cost
